fe89ad5d084f9378e9e2936af3f0397b4e7c3c77
sidra39 <sidrabibi4675@gmail.com>
2026-09-09 07:10:37 +0500

    Update frontend

--- lib/features/notifications/services/notification_provider.dart
+++ lib/features/notifications/services/notification_provider.dart
@@ -81,8 +81,4 @@ class NotificationProvider extends ChangeNotifier {
       await fetchNotifications(token);
     } catch (_) {}
   }
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> 933e2ef9672b79d50dcca3f1bc1666d87b0e4a02

4d2b53d0c874d136e3d64118854f61ed74895478
sidra39 <sidrabibi4675@gmail.com>
2026-09-09 06:42:49 +0500

    Update frontend

--- lib/core/widgets/notification_bell_icon.dart
+++ lib/core/widgets/notification_bell_icon.dart
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import '../../features/notifications/screens/notifications_screen.dart';
@@ -58,6 +57,3 @@ class NotificationBellIcon extends StatelessWidget {
     );
   }
 }
-=======
-
->>>>>>> 933e2ef9672b79d50dcca3f1bc1666d87b0e4a02

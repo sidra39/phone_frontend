f30c85b96dae9165ff45f3434af70dfbd7b0db2a
sidra39 <sidrabibi4675@gmail.com>
2026-09-09 07:12:19 +0500

    Update frontend

--- lib/features/reports/services/report_service.dart
+++ lib/features/reports/services/report_service.dart
@@ -33,8 +33,4 @@ class ReportService {
     final List list = response['data'] ?? [];
     return list.map((item) => ReportModel.fromJson(item)).toList();
   }
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> 933e2ef9672b79d50dcca3f1bc1666d87b0e4a02

7301b7dd08ed6d4c321a5861967d521ccd18d397
sidra39 <sidrabibi4675@gmail.com>
2026-09-09 07:03:08 +0500

    Update frontend

--- lib/features/browse/services/browse_service.dart
+++ lib/features/browse/services/browse_service.dart
@@ -56,8 +56,4 @@ class BrowseService {
     final List list = response['data'] ?? [];
     return list.map((item) => CategoryModel.fromJson(item)).toList();
   }
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> 933e2ef9672b79d50dcca3f1bc1666d87b0e4a02

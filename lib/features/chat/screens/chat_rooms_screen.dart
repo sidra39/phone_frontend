cd8c1552a75448b2695bcb8426490b01f1fd2c5a
sidra39 <sidrabibi4675@gmail.com>
2026-09-09 07:04:19 +0500

    Update frontend

--- lib/features/chat/screens/chat_rooms_screen.dart
+++ lib/features/chat/screens/chat_rooms_screen.dart
@@ -176,8 +176,4 @@ class _ChatRoomsScreenState extends State<ChatRoomsScreen> {
                 ),
     );
   }
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> 933e2ef9672b79d50dcca3f1bc1666d87b0e4a02
--- lib/features/chat/screens/chat_screen.dart
+++ lib/features/chat/screens/chat_screen.dart
@@ -313,8 +313,4 @@ class _ChatScreenState extends State<ChatScreen> {
       ),
     );
   }
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> 933e2ef9672b79d50dcca3f1bc1666d87b0e4a02
